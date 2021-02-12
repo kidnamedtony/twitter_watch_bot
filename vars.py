2a30f3944befc2cbd42ffad9d9b0577f38ded247
Tony Loiseleur <a.loiseleur@gmail.com>
2021-02-12 14:29:34 -0800

    add supplementary files necessary for running twitter_watch_bot.py

---
+++ vars.py
@@ -0,0 +1,7 @@
+# These are the variables you want the Twitter bot to watch out for. Feel free to replace or add
+# to these terms with watch words of your own!
+watch_lst = ["replace", "these", "test", "terms", "with", "something", "else"]
+
+# NOTE: just in case you're wondering, case doesn't matter. The way that the twitter_watch_bot
+# is currently designed, it sets all terms (both in the watch_lst and in the tweets it watches)
+# to lower case before matching and fuzzy-matching them.
